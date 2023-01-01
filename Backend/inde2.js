90bc3810d11517093fe8a38d43c34be48c55a45d
Adarsh <adarshgupta2468@gmail.com>
2023-01-01 15:54:50 +0530

    server

--- Backend/inde2.js
+++ Backend/inde2.js
@@ -290,4 +290,4 @@ app.post("/likes", async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log("Server listening on port 3000!"));
+app.listen(process.env.PORT || 3000, () => console.log("Server listening on port 3000!"));
